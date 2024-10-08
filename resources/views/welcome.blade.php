0f2d892cd523c786134a28e17801dbe19470a016
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-08 14:00:04 +0300

    Add Post model, factory, migration ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,27 @@
 </head>
 
 <body>
-
+    <div class="container mx-auto">
+        <div class="grid grid-cols-4 gap-4">
+            @for($i=0;$i<16;$i++)
+                <div>
+                    <div class="card bg-base-100 shadow-xl">
+                        <figure>
+                            <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Shoes! {{ $i }}</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+        </div>
+    </div>
 </body>
 
 </html>
