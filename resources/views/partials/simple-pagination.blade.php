dfafd79364763a1981ee97388e9783e971e9ab86
Artemiy <118753592+kykyxartemij@users.noreply.github.com>
2025-01-14 13:12:16 +0200

    simple-paginate 2nd change

--- resources/views/partials/simple-pagination.blade.php
+++ resources/views/partials/simple-pagination.blade.php
@@ -12,31 +12,6 @@
                 </a>
             @endif
 
-            {{-- Pagination Elements --}}
-            @foreach ($elements as $element)
-                {{-- "Three Dots" Separator --}}
-                @if (is_string($element))
-                    <button class="btn btn-disabled" aria-disabled="true">
-                        {{ $element }}
-                    </button>
-                @endif
-
-                {{-- Array Of Links --}}
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <button class="btn btn-active">
-                                {{ $page }}
-                            </button>
-                        @else
-                            <a href="{{ $url }}" class="btn">
-                                {{ $page }}
-                            </a>
-                        @endif
-                    @endforeach
-                @endif
-            @endforeach
-
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
                 <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="btn">
